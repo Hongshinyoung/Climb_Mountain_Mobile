44ea2747e01893d8b3722b7e5312def36c4e833f
Hongshinyoung <88022654+Hongshinyoung@users.noreply.github.com>
2024-04-02 11:53:46 +0900

    작업중
    작업중인파일 옮김

---
+++ Assets/MY/Scripts/Item.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Create New Item")]
+public class Item : MonoBehaviour
+{
+    public int id;
+    public string itemName;
+    public string description;
+    public Sprite icon;
+
+}
