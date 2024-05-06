f8e850a75c87a77d5ddef09eb2dec3235a71105c
Hongshinyoung <88022654+Hongshinyoung@users.noreply.github.com>
2024-05-07 00:55:22 +0900

    인벤토리 저장 기능
    feat: 인벤토리 저장 기능 추가(추후 스테이지 저장 및 랭킹 시스템 작업예정)

--- Assets/MY/Scripts/DataManager.cs
+++ Assets/MY/Scripts/DataManager.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
     public static DataManager Instance { get; private set; }
 
-    private const string saveFileName = "gold_data.json"; // JSON 파일 이름
+    private const string saveFileGold = "gold_data.json"; // JSON 파일 이름
+    private const string saveFileInven = "inven_data.json";
 
     private void Awake()
     {
@@ -17,12 +19,30 @@ public class DataManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject); // DataManager 오브젝트를 다음 씬으로 전달합니다.
     }
+
+    public void SaveStageData()
+    {
+
+    }
     public void SaveInventoryData() //인벤 -> json
     {
         List<Item> invenData = new List<Item>();
         invenData = InventoryManager.Instance.items;
         string jsonData = JsonUtility.ToJson(invenData);
-        File.WriteAllText(saveFileName, jsonData);
+        File.WriteAllText(saveFileInven, jsonData);
+        SaveToCloud(jsonData);
+    }
+
+    public void LoadInventoryData() //json -> 인벤
+    {
+        string filePath = GetSaveInvenPath();
+        List<Item> invenData = new List<Item>();
+        if(File.Exists(saveFileInven))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            invenData = JsonUtility.FromJson<List<Item>>(jsonData);
+            LoadFromCloud(saveFileInven);
+        }
     }
 
     public void SaveGoldData() // gold -> json
@@ -52,16 +72,22 @@ public class DataManager : MonoBehaviour
 
     private string GetSaveFilePath()
     {
-        return Path.Combine(Application.persistentDataPath, saveFileName);
+        return Path.Combine(Application.persistentDataPath, saveFileGold);
+    }
+    private string GetSaveInvenPath()
+    {
+        return Path.Combine(Application.persistentDataPath + saveFileInven);
     }
 
     private void SaveToCloud(string jsonData)
     {
-        GPGSBinder.Inst.SaveCloud(saveFileName, jsonData);
+        GPGSBinder.Inst.SaveCloud(saveFileGold, jsonData);
+        GPGSBinder.Inst.SaveCloud(saveFileInven, jsonData);
     }
     private void LoadFromCloud(string saveFileName)
     {
         GPGSBinder.Inst.LoadCloud(saveFileName);
+        GPGSBinder.Inst.LoadCloud(saveFileInven);
     }
 }
 
